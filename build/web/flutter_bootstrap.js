94c95834df87cde84dbafe978ee4a74bb519c0e5
jchavezshn <jchavezshn@gmail.com>
2025-03-09 18:26:13 -0600

    modified:   build/web/flutter_bootstrap.js 	modified:   build/web/flutter_service_worker.js 	modified:   build/web/index.html 	modified:   build/web/main.dart.js

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"83bacfc52569459a4a654727cad2546820cb0d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2015108352"
+    serviceWorkerVersion: "2801559551"
   }
 });
