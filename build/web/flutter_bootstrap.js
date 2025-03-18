86fe85a1646cbc8f91032cb31264780375e2283f
jchavezshn <jchavezshn@gmail.com>
2025-03-17 23:23:11 -0600

    modified:   .DS_Store 	modified:   build/web/assets/fonts/MaterialIcons-Regular.otf 	modified:   build/web/flutter_bootstrap.js 	modified:   build/web/flutter_service_worker.js 	modified:   build/web/index.html 	modified:   build/web/main.dart.js

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"83bacfc52569459a4a654727cad2546820cb0d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "479126287"
+    serviceWorkerVersion: "411410433"
   }
 });
